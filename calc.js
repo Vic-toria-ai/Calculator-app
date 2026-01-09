43030e0acf01883f66743a2a4d1b774a038eed9f
Vic-toria-ai <vickalu860@gmail.com>
2026-01-09 11:34:41 +0000

    initial commit

---
+++ calc.js
@@ -0,0 +1,19 @@
+// interactive calculator
+ const display = document.getElementById("display");
+
+ function addToDisplay(input){
+ display.value += input;
+ } 
+
+ function clearDisplay(){
+   display.value = "";
+ }
+
+ function calc(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value ="Error";
+ }
+}
